7f1cc9188a910d51e7da2cc09b61ef4c3342ea66
Vaibhavi <g.vaibhavi410@gmail.com>
2026-01-12 14:40:00 +0530

    Sum of Unique Elments

---
+++ 1748.java
@@ -0,0 +1,16 @@
+public class Solution{
+    public int sumOfUnique(int[] nums){
+        int [] freq=new int[101];
+        for(int i=0;i<nums.length;i++){
+            freq[nums[i]]++;
+        }
+        int sum=0;
+        for(int i=0;i<=100;i++){
+            if(freq[i]==1){
+               sum+=i;
+            }
+            
+        }
+        return sum ;
+    }
+}
